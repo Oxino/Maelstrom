84bfc8ba2cfea2f9296b02cea628583d13ba3166
nomuser <user@gmail.com>
2022-07-13 09:14:14 +0200

    update reco card

---
+++ lib/widgets/home/reco_card.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:maelstrom/bloc/storage.dart';
+import 'package:maelstrom/models/business_model.dart';
+import 'package:maelstrom/models/event_model.dart';
+import 'package:maelstrom/repositories/business/business_repo.dart';
+import 'package:maelstrom/widgets/base_button.dart';
+import 'package:maelstrom/widgets/base_text.dart';
+
+import 'package:maelstrom/config.dart';
+
+import 'package:maelstrom/widgets/tags_widget.dart';
+import 'package:maelstrom/widgets/base_image.dart';
+
+class RecoCard extends StatelessWidget {
+  final EventModel event;
+  RecoCard(this.event);
+  final Storage _firestoreStorage = Storage();
+  final BusinessRepo _businessRepos = BusinessRepo();
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(8),
+      decoration: BoxDecoration(
+          color: ThemeColors.grayColor,
+          borderRadius: BorderRadius.all((Radius.circular(10)))),
+      child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            BaseImage(ImageType.reco, event.imageName),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(height: 8),
+                BaseText(TextType.littleBoldText, event.name, textMaxSize: 40),
+                SizedBox(height: 8),
+                StreamBuilder<BusinessModel?>(
+                    stream: _businessRepos.getCurrentBusiness(event.idBusiness),
+                    builder: ((context, snapshot) {
+                      if (!snapshot.hasData)
+                        return BaseText(
+                            TextType.littleText, "Pas d'établissement",
+                            textMaxSize: 30);
+                      return BaseText(
+                          TextType.littleText, snapshot.data!.institutionName,
+                          textMaxSize: 30);
+                    })),
+                SizedBox(height: 8),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: event.tags.map<Widget>((tag) {
+                          if (event.tags.indexOf(tag) > 3) return Container();
+                          return TagsWidget(TagsType.bubble, tag['name'],
+                              Color(tag['colorValue']));
+                        }).toList()),
+                    BaseButton(ButtonsType.icon, () {}),
+                  ],
+                )
+              ],
+            )
+          ]),
+    );
+  }
+}
--- lib/widgets/home/reco_section.dart
+++ lib/widgets/home/reco_section.dart
@@ -1,105 +1,41 @@
 import 'package:flutter/material.dart';
-import 'package:maelstrom/widgets/base_button.dart';
+import 'package:maelstrom/models/event_model.dart';
+import 'package:maelstrom/repositories/event/event_repo.dart';
 import 'package:maelstrom/widgets/base_text.dart';
 
 import 'package:maelstrom/config.dart';
-
-import 'package:maelstrom/widgets/tags_widget.dart';
-import 'package:maelstrom/widgets/base_image.dart';
+import 'package:maelstrom/widgets/home/reco_card.dart';
 
 class RecoSection extends StatelessWidget {
-  final List recoList = [
-    {
-      'title': 'Soirée célib : Faites des rencontre bla blab labfnzzkljj',
-      'place': 'La casacasa casacasa casa casacasacasac ',
-      'picture': 'assets/images/image1.jpg',
-      'km': '1km',
-      'tag': [],
-    },
-    {
-      'title': 'Tournois Super Smash Bros Ulitimate',
-      'place': 'Le R4dom',
-      'picture': 'assets/images/image2.jpg',
-      'km': '1.5km',
-      'tag': [],
-    },
-    {
-      'title': 'Soirée aèvec DJ Snake  1 conso offerte',
-      'place': 'La plage',
-      'picture': 'assets/images/image3.jpg',
-      'km': '2km',
-      'tag': [],
-    },
-    {
-      'title': 'Soirée Beer Pong',
-      'place': 'La Grange',
-      'picture': 'assets/images/image2.jpg',
-      'km': '2km',
-      'tag': [],
-    },
-    {
-      'title': 'Rencontre Geek',
-      'place': 'Le Sherlock',
-      'picture': 'assets/images/image1.jpg',
-      'km': '2.5km',
-      'tag': [],
-    },
-  ];
   @override
   Widget build(BuildContext context) {
-    return GridView(
-        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2,
-          crossAxisSpacing: 15,
-          mainAxisSpacing: 10,
-          mainAxisExtent: 230,
-        ),
-        shrinkWrap: true,
-        physics: ScrollPhysics(),
-        children: recoList.map((reco) {
-          return RecoCard(reco);
-        }).toList());
-  }
-}
-
-class RecoCard extends StatelessWidget {
-  final Map recoData;
-  RecoCard(this.recoData);
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.all(8),
-      decoration: BoxDecoration(
-          color: ThemeColors.grayColor,
-          borderRadius: BorderRadius.all((Radius.circular(10)))),
-      child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            BaseImage(ImageType.reco, recoData['picture'], recoData['km']),
-            SizedBox(height: 8),
-            BaseText(TextType.littleBoldText, recoData['title'],
-                textMaxSize: 40),
-            SizedBox(height: 8),
-            BaseText(TextType.littleText, recoData['place'], textMaxSize: 30),
-            SizedBox(height: 8),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    TagsWidget(TagsType.bubble, "test", TagsColors.yellowTag),
-                    SizedBox(width: 5),
-                    TagsWidget(TagsType.bubble, "test", TagsColors.blueTag),
-                    SizedBox(width: 5),
-                    TagsWidget(TagsType.bubble, "test", TagsColors.redTag),
-                  ],
-                ),
-                BaseButton(ButtonsType.icon, () {}),
-              ],
-            )
-          ]),
-    );
+    final EventRepos _eventRepos = EventRepos();
+    return StreamBuilder<List<EventModel?>>(
+        stream: _eventRepos.getTodayEvents([]),
+        builder: ((context, snapshot) {
+          if (!snapshot.hasData)
+            return Container(
+              child: BaseText(
+                TextType.bodyBoldText,
+                "Pas d'évènement aujourd'hui",
+              ),
+            );
+          return GridView(
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                crossAxisSpacing: 15,
+                mainAxisSpacing: 10,
+                mainAxisExtent: 230,
+              ),
+              shrinkWrap: true,
+              physics: ScrollPhysics(),
+              children: snapshot.data!.map((event) {
+                if (event != null) {
+                  return RecoCard(event);
+                } else {
+                  return Container();
+                }
+              }).toList());
+        }));
   }
 }
