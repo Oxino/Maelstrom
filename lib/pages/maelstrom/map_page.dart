e278d6afbab2cb4716654982c06e2dbe9df4a259
nomuser <user@gmail.com>
2022-07-13 09:10:41 +0200

    test with map

--- lib/pages/maelstrom/map_page.dart
+++ lib/pages/maelstrom/map_page.dart
@@ -1,12 +1,138 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:geocoding/geocoding.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+// import 'package:mapbox_gl/mapbox_gl.dart';
 
-class MapPage extends StatelessWidget {
+class MapPage extends StatefulWidget {
+  MapPage({Key? key}) : super(key: key);
+
+  @override
+  State<MapPage> createState() => _MapPageState();
+}
+
+class _MapPageState extends State<MapPage> {
   late GoogleMapController mapController;
-
   final LatLng _center = const LatLng(44.837789, -0.57918);
   bool isMapCreated = false;
+
+  late Position _currentPosition;
+  String _currentAddress = '';
+
+  Set<Marker> markers = {};
+
+  final startAddressController = TextEditingController();
+
+  String _startAddress = '';
+  String _destinationAddress = '';
+
+  @override
+  void initState() {
+    super.initState();
+    _getCurrentLocation();
+  }
+
+  // Method for retrieving the current location
+
+  _getCurrentLocation() async {
+    await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high)
+        .then((Position position) async {
+      setState(() {
+        // Store the position in the variable
+        _currentPosition = position;
+
+        print('CURRENT POS: $_currentPosition');
+
+        // For moving the camera to current location
+        mapController.animateCamera(
+          CameraUpdate.newCameraPosition(
+            CameraPosition(
+              target: LatLng(position.latitude, position.longitude),
+              zoom: 18.0,
+            ),
+          ),
+        );
+      });
+      await _getAddress();
+    }).catchError((e) {
+      print(e);
+    });
+  }
+  // Method for retrieving the address
+
+  _getAddress() async {
+    try {
+      // Places are retrieved using the coordinates
+      List<Placemark> p = await placemarkFromCoordinates(
+          _currentPosition.latitude, _currentPosition.longitude);
+
+      // Taking the most probable result
+      Placemark place = p[0];
+
+      setState(() {
+        // Structuring the address
+        _currentAddress =
+            "${place.name}, ${place.locality}, ${place.postalCode}, ${place.country}";
+
+        // Update the text of the TextField
+        startAddressController.text = _currentAddress;
+
+        // Setting the user's present location as the starting address
+        _startAddress = _currentAddress;
+      });
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  Future<bool> _calculateDistance() async {
+    try {
+      // Retrieving placemarks from addresses
+      List<Location> startPlacemark = await locationFromAddress(_startAddress);
+      List<Location> destinationPlacemark =
+          await locationFromAddress(_destinationAddress);
+
+// Storing latitude & longitude of start and destination location
+      double startLatitude = startPlacemark[0].latitude;
+      double startLongitude = startPlacemark[0].longitude;
+      double destinationLatitude = destinationPlacemark[0].latitude;
+      double destinationLongitude = destinationPlacemark[0].longitude;
+
+      String startCoordinatesString = '($startLatitude, $startLongitude)';
+      String destinationCoordinatesString =
+          '($destinationLatitude, $destinationLongitude)';
+
+// Start Location Marker
+      Marker startMarker = Marker(
+        markerId: MarkerId(startCoordinatesString),
+        position: LatLng(startLatitude, startLongitude),
+        infoWindow: InfoWindow(
+          title: 'Start $startCoordinatesString',
+          snippet: _startAddress,
+        ),
+        icon: BitmapDescriptor.defaultMarker,
+      );
+
+// Destination Location Marker
+      Marker destinationMarker = Marker(
+        markerId: MarkerId(destinationCoordinatesString),
+        position: LatLng(destinationLatitude, destinationLongitude),
+        infoWindow: InfoWindow(
+          title: 'Destination $destinationCoordinatesString',
+          snippet: _destinationAddress,
+        ),
+        icon: BitmapDescriptor.defaultMarker,
+      );
+
+      // Add the markers to the list
+      markers.add(startMarker);
+      markers.add(destinationMarker);
+    } catch (e) {
+      print(e);
+    }
+    return false;
+  }
 
   void _onMapCreated(GoogleMapController controller) {
     mapController = controller;
@@ -33,7 +159,7 @@ class MapPage extends StatelessWidget {
     }
     return GoogleMap(
       onMapCreated: _onMapCreated,
-      myLocationButtonEnabled: false,
+      myLocationButtonEnabled: true,
       // gestureRecognizers: Set()
       //   ..add(Factory<PanGestureRecognizer>(() => PanGestureRecognizer())),
       zoomControlsEnabled: false,
@@ -41,6 +167,35 @@ class MapPage extends StatelessWidget {
         target: _center,
         zoom: 13.0,
       ),
+      markers: Set<Marker>.from(markers),
     );
   }
 }
+
+// class MapPage extends StatefulWidget {
+//   MapPage({Key? key}) : super(key: key);
+
+//   @override
+//   State<MapPage> createState() => _MapPageState();
+// }
+
+// class _MapPageState extends State<MapPage> {
+//   MapboxMapController? mapController;
+//   var isLight = true;
+
+//   _onMapCreated(MapboxMapController controller) {
+//     mapController = controller;
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MapboxMap(
+//       styleString: MapboxStyles.DARK,
+//       // accessToken: MapsDemo.ACCESS_TOKEN,
+//       onMapCreated: _onMapCreated,
+//       initialCameraPosition:
+//           const CameraPosition(target: LatLng(44.837789, -0.57918)),
+//       // onStyleLoadedCallback: _onStyleLoadedCallback,
+//     );
+//   }
+// }
